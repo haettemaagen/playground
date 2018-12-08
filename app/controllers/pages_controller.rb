96d4f4ebfdd5b9c4dcf1843870a382158bd4576b
Nicolas Feer <atmosfeer@gmail.com>
2018-12-08 11:59:21 +0100

    added CSS Gridstuff

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,7 @@
 class PagesController < ApplicationController
   def home
   end
+
+  def bootstrap
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
   root 'pages#home'
+  get 'bootstrap', to: 'pages#bootstrap', as: :bootstrap
+  get 'cssgrid', to: 'pages#cssgrid', as: :cssgrid
 end
