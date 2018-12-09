412ec2b0d0ce2620bb24be244ca2604c171ebecb
Christian Wienberg <christianwienberg@live.dk>
2018-12-09 11:32:24 +0100

    CSS Grid

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -4,4 +4,6 @@ class PagesController < ApplicationController
 
   def bootstrap
   end
+  def dashboard
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   root 'pages#home'
   get 'bootstrap', to: 'pages#bootstrap', as: :bootstrap
   get 'cssgrid', to: 'pages#cssgrid', as: :cssgrid
+  get 'dashboard', to: 'pages#dashboard', as: :dashboard
 end
